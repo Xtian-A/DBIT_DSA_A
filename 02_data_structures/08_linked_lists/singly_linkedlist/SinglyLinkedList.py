249fa29e50890e9d2265c154174cef8477aaa688
Xtian-A <xtian.aligizimba@gmail.com>
2025-06-09 09:29:26 +0300

    added comments and merged

--- 02_data_structures/08_linked_lists/singly_linkedlist/SinglyLinkedList.py
+++ 02_data_structures/08_linked_lists/singly_linkedlist/SinglyLinkedList.py
@@ -6,21 +6,21 @@ class LinkedList:
         self.tail = None
         self.size = 0
 
-    def append(self, data):
+    def append(self, data): #Append used to add node. Method defined for linked list.
         node = Node(data)  # Create a new node, pass the value to be be appended
-        if self.tail:
-            self.tail.next = node
+        if self.tail: #If not sure your item is head or tail, use this to check if it exists. If tail exists, then what is added is to the tail. And make the new node the tail.
+            self.tail.next = node  
             self.tail = node
         else:
             # if there are no elements then the new node becomes the head and tail.
             self.head = node
             self.tail = node
 
-        self.size += 1
+        self.size += 1 #This counts how many nodes in linked list. Instead of counting, checks the size. 
 
     def append_at_a_location(self, data, index):
         # check for invalid input.
-        if index < 0 or index > self.size:
+        if index < 0 or index > self.size: #Trying to insert the value. But cannot be inserted at index -1 or is not greater than maximum number of nodes -invalid node.
             print("Invalid index")
             return
 
@@ -36,21 +36,21 @@ class LinkedList:
             current = self.head
             for i in range(index - 1):
                 current = current.next
-            node.next = current.next
+            node.next = current.next #Makes the node if inserting will become the new head, Inserting the value at index 0.
             current.next = node
             if node.next is None:
                 self.tail = node
 
         self.size += 1
 
-    def display(self):
+    def display(self): #to display the nodes
         current = self.head
         while current:
             print(current.data, end=" -> ")
             current = current.next
         print("None")
 
-    def search(self, data):
+    def search(self, data): #when searching for specific node, value
         current = self.head
         index = 0
         while current:
@@ -60,12 +60,12 @@ class LinkedList:
             index += 1
         return -1  # Return -1 when data is not found
 
-    def delete(self, data):
+    def delete(self, data): #deleting a node. And makes sure the node after deleted, the previous points to the next value. 
         current = self.head
         prev = None
 
         while current:
-            if current.data == data:
+            if current.data == data: #if current value is what we want to delete, then...
                 if prev:
                     prev.next = current.next
                 else:
--- 02_recursion/qst.py
+++ 02_recursion/qst.py
@@ -1,1 +1,54 @@
 print("hello world")
+#def new__func():
+#    new_func() #function calling itself. has no base case and stops with recursion error or stack upto 1000.
+
+#def new_func(n):
+
+#    if n > 1:
+#        print("Hello")
+#        new_func(n - 1) #function calling itself -direct recursion
+                         #tail recursion since pulling values as we go from 5 to 2
+
+
+#new_func(5)#calling the function for first time and passing number 5. The function above recieves it. omitting this,nothing displayed
+
+#def new_func(n):
+
+#    if n > 1:
+#        print(f"Value of n is (n)")
+#        new_func(n - 1) 
+
+
+#new_func(5)
+
+def new_func(n):
+
+    if n == 0:
+        #print("Stop")
+        return 0
+    else:
+        print("Hello")
+        new_func(n - 1)#function calling itself
+
+
+new_func(5)
+
+def head_recursion(num):
+    if n == 0:
+        return 0
+    head_recursion(num - 1)
+    print (f"(num) Head Recursion")
+    return None
+
+
+head_recursion(12)
+#from 1-12 because of the stack. The function is calling itself before calling the values. Store the values first  (12 to 1), then pull the values (1 to 12).
+#last in last out
+
+def binary(n):
+    print(n)
+    if n > 0:
+        binary(n//2)#// rounds numbers off. avoids decimal places
+#typically use recursion for binary search 
+
+binary(1_000_000_000)
